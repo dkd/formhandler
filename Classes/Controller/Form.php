acaf9f8abb3ee0def9cf7eb0fdefe4496ffc08b1
Andreas Beyl <ab.gitlab@manywaysout.de>
2017-05-24 15:22:34 +0200

    [TASK] change instanciation of PageRenderer

--- Classes/Controller/Form.php
+++ Classes/Controller/Form.php
@@ -1465,7 +1465,7 @@ class Form extends AbstractController
             $file = $fileOptions['file'];
             if (strlen(trim($file)) > 0) {
                 $file = $this->utilityFuncs->resolveRelPathFromSiteRoot($file);
-                $pageRenderer = $GLOBALS['TSFE']->getPageRenderer();
+                $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
                 $pageRenderer->addJsFile(
                     $file,
                     $fileOptions['type'] ? $fileOptions['type'] : 'text/javascript',
@@ -1490,7 +1490,7 @@ class Form extends AbstractController
             $file = $fileOptions['file'];
             if (strlen(trim($file)) > 0) {
                 $file = $this->utilityFuncs->resolveRelPathFromSiteRoot($file);
-                $pageRenderer = $GLOBALS['TSFE']->getPageRenderer();
+                $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
                 $pageRenderer->addJsFooterFile(
                     $file,
                     $fileOptions['type'] ? $fileOptions['type'] : 'text/javascript',
